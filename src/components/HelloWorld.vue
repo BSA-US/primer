fb716bb9fcb7fdbd46a2a983b525a5542323219f
replygirl <moon@moon.fyi>
2020-06-04 23:48:29 -0400

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template lang='pug'>
+  .hello
+    h1 {{ msg }}
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template lang='pug'>
+  .home
+    img(alt='Vue logo' src='../assets/logo.png')
+    HelloWorld(msg='Welcome to Your Vue.js App')
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
