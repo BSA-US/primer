9d0413001220635efe6423e4781ff46a11dba9dc
replygirl <moon@moon.fyi>
2020-06-11 21:29:12 -0400

    how about no?

--- src/env.js
+++ src/env.js
@@ -1,7 +1,11 @@
 export const api = {
-  baseUrl: process.env.VUE_APP_API_BASEURL,
+  baseUrl:
+    process.env.VUE_APP_API_BASEURL
+    || 'https://primer-git-dev.blacksocialists.now.sh/api',
   endpoints: {
-    config: process.env.VUE_APP_API_ENDPOINT_CONFIG
+    config:
+      process.env.VUE_APP_API_ENDPOINT_CONFIG
+      || '/config',
   }
 }
 
