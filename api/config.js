ff54750fe18f9ca3b6317776a9205a708a93c258
replygirl <moon@moon.fyi>
2020-06-20 17:57:53 -0400

    refactor(api): wrap config helpers in promise.all

--- api/config.js
+++ api/config.js
@@ -1,9 +1,9 @@
 import a from './_airtable'
 
-export default async (_req, res) =>
-  res.send({
-    ...await a('Links'),
-    ...await a('Nodes'),
-    ...await a('Pillars'),
-    ...await a('Steps')
-  })
+export default async (_req, res) => {
+  const tables = await Promise.all(
+    ['Links', 'Nodes', 'Pillars', 'Steps'].map(x => a(x))
+  )
+
+  res.send(tables.reduce((acc, cur) => ({ ...acc, ...cur }), {}))
+}
